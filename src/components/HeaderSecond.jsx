50826707fe5f220ee95c8c1d7708b4b8e65a68c8
ritu-477 <ritujoharbishnoi@gmail.com>
2024-10-15 10:43:59 +0530

    update done

--- src/components/HeaderSecond.jsx
+++ src/components/HeaderSecond.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import navLogo from '../../assets/images/webp/nav-logo.webp';
-import { NAV_ITEMS } from '../../utils/Helper'
 
 const HeaderSecond = () => {
     const [open, setOpen] = useState(false);
--- src/utils/Helper.jsx
+++ src/utils/Helper.jsx
@@ -1,9 +1,9 @@
 
 export const NAV_ITEMS = [
-    { href: "#home", text: "TAB NAME" },
-    { href: "#about", text: "TAB NAME" },
-    { href: "#tab", text: "TAB NAME" },
-    { href: "#faq", text: "TAB NAME" },
+    { href: "#", text: "TAB NAME" },
+    { href: "#", text: "TAB NAME" },
+    { href: "#", text: "TAB NAME" },
+    { href: "#", text: "TAB NAME" },
     
 ];
 
