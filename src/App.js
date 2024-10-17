ac6335bffbf9fea99e26a6215f48d8228c83c452
Ankush307 <ankushdhull966@gmail.com>
2024-10-17 16:02:52 +0530

    about section done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { AboutFirst } from './components/AboutFirst';
+import { AboutSecond } from './components/AboutSecond';
+import { AboutThird } from './components/AboutThird';
+import { AboutForth } from './components/AboutForth';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AboutFirst/>
+      <AboutSecond/>
+      <AboutThird/>
+      <AboutForth/>
     </div>
   );
 }
