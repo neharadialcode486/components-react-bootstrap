6e0c0a262819cb733fa07a8a7711fe79f7bde53c
anjali <anjalichanda2019@gmail.com>
2024-10-22 15:57:12 +0530

    add lenis smooth scroll

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BackToTop from './components/BackToTop';
+import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
 
 function App() {
+  const lenis = useLenis(({ scroll }) => {
+    console.log("Scrolling is happening!", scroll);
+  })
   return (
     <>
-      <div className='bg-black min-vh-100'></div>
-      <div className='bg-danger min-vh-100'></div>
-      <div className='bg-orange-500 min-vh-100'></div>
-      <div className='bg-info min-vh-100'></div>
-      <BackToTop />
+      <ReactLenis root>
+        {/* Add your components here */}
+        <div className='bg-black min-vh-100'></div>
+        <div className='bg-danger min-vh-100'></div>
+        <div className='bg-orange-500 min-vh-100'></div>
+        <div className='bg-info min-vh-100'></div>
+        <div className='bg-black min-vh-100'></div>
+        <div className='bg-danger min-vh-100'></div>
+      </ReactLenis>
     </>
   );
 }
